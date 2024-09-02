045a776bad6c2dedd962283802b2c9e66529fe25
jurijo <jurijo@yandex-team.ru>
2024-09-03 02:56:35 +0300

    sql

--- dao/dao.go
+++ dao/dao.go
@@ -2,8 +2,90 @@
 
 package dao
 
-import "context"
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+type MyDao struct {
+	db *sql.DB
+}
 
 func CreateDao(ctx context.Context, dsn string) (Dao, error) {
-	panic("not implemented")
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	_, err = db.ExecContext(ctx, `
+CREATE TABLE dao (
+    ID SERIAL PRIMARY KEY,
+    Name text
+)`)
+	if err != nil {
+		return &MyDao{db: db}, err
+	}
+	return &MyDao{db: db}, nil
+}
+
+func (d *MyDao) Create(ctx context.Context, u *User) (UserID, error) {
+	var id int
+	err := d.db.QueryRowContext(ctx, `INSERT INTO dao (Name) VALUES ($1) RETURNING ID`, u.Name).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+	fmt.Println(u.Name, id)
+	return UserID(id), nil
+}
+
+func (d *MyDao) Update(ctx context.Context, u *User) error {
+	res, err := d.db.ExecContext(ctx, `UPDATE dao SET Name = $1 WHERE ID = $2`, u.Name, u.ID)
+	if err != nil {
+		return err
+	}
+	cnt, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if cnt == 0 {
+		return errors.New("no found id")
+	}
+	return err
+}
+
+func (d *MyDao) Delete(ctx context.Context, id UserID) error {
+	_, err := d.db.ExecContext(ctx, `DELETE FROM dao WHERE ID = $1`, id)
+	return err
+}
+
+func (d *MyDao) Lookup(ctx context.Context, id UserID) (User, error) {
+	user := User{ID: id}
+	err := d.db.QueryRowContext(ctx, `SELECT Name FROM dao WHERE ID = $1`, id).Scan(&user.Name)
+	return user, err
+}
+
+func (d *MyDao) List(ctx context.Context) ([]User, error) {
+	rows, err := d.db.QueryContext(ctx, `SELECT Name, ID FROM dao`)
+	if err != nil {
+		return nil, err
+	}
+	var users []User
+	for rows.Next() {
+		users = append(users, User{})
+		err := rows.Scan(&users[len(users)-1].Name, &users[len(users)-1].ID)
+		if err != nil {
+			return users, err
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return users, nil
+	}
+	return users, nil
+}
+
+func (d *MyDao) Close() error {
+	err := d.db.Close()
+	return err
 }
--- ledger/ledger.go
+++ ledger/ledger.go
@@ -2,8 +2,122 @@
 
 package ledger
 
-import "context"
+import (
+	"context"
+	"database/sql"
+	"errors"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+type ledger struct {
+	db *sql.DB
+}
+
+func (l *ledger) Close() error {
+	return l.db.Close()
+}
 
 func New(ctx context.Context, dsn string) (Ledger, error) {
-	panic("not implemented")
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.ExecContext(ctx, `
+CREATE TABLE ledger (
+    ID text UNIQUE,
+    Money integer
+)`)
+	return &ledger{db: db}, err
+}
+
+func (l *ledger) CreateAccount(ctx context.Context, id ID) error {
+	_, err := l.db.ExecContext(ctx, `INSERT INTO ledger (ID, Money) VALUES ($1, 0)`, id)
+	return err
+}
+
+func (l *ledger) GetBalance(ctx context.Context, id ID) (Money, error) {
+	var money Money
+	err := l.db.QueryRowContext(ctx, `SELECT Money FROM ledger WHERE ID = $1`, id).Scan(&money)
+	return money, err
+}
+
+func (l *ledger) Deposit(ctx context.Context, id ID, amount Money) error {
+	if amount < 0 {
+		return ErrNegativeAmount
+	}
+	res, err := l.db.ExecContext(ctx, `UPDATE ledger SET Money = Money + $1 WHERE ID = $2`, amount, id)
+	if err != nil {
+		return err
+	}
+	cnt, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if cnt == 0 {
+		return errors.New("no found id in database")
+	}
+	return nil
+}
+
+func (l *ledger) Withdraw(ctx context.Context, id ID, amount Money) error {
+	if amount < 0 {
+		return ErrNegativeAmount
+	}
+	var curMoney Money
+	tx, err := l.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+	if err := tx.QueryRowContext(ctx, `SELECT money FROM ledger WHERE ID = $1 FOR UPDATE`, id).Scan(&curMoney); err != nil {
+		return err
+	}
+	if curMoney-amount < 0 {
+		return ErrNoMoney
+	}
+	_, err = tx.ExecContext(ctx, `UPDATE ledger SET Money = Money - $1 WHERE ID = $2`, amount, id)
+	return tx.Commit()
+}
+
+func (l *ledger) Transfer(ctx context.Context, from, to ID, amount Money) error {
+	if amount < 0 {
+		return ErrNegativeAmount
+	}
+	var amountFrom Money
+	tx, err := l.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	if from < to {
+		if err := tx.QueryRowContext(ctx, `SELECT money FROM ledger WHERE ID = $1 FOR UPDATE`, from).Scan(&amountFrom); err != nil {
+			return err
+		}
+		var tmp Money
+		if err := tx.QueryRowContext(ctx, `SELECT money FROM ledger WHERE ID = $1 FOR UPDATE `, to).Scan(&tmp); err != nil {
+			return err
+		}
+	} else {
+		if _, err := tx.ExecContext(ctx, `SELECT money FROM ledger WHERE ID = $1 FOR UPDATE `, to); err != nil {
+			return err
+		}
+		if err := tx.QueryRowContext(ctx, `SELECT money FROM ledger WHERE ID = $1 FOR UPDATE`, from).Scan(&amountFrom); err != nil {
+			return err
+		}
+	}
+	if amountFrom-amount < 0 {
+		return ErrNoMoney
+	}
+	if _, err := tx.ExecContext(ctx, `UPDATE ledger SET Money = Money - $1 WHERE ID = $2`, amount, from); err != nil {
+		return err
+	}
+	if _, err := tx.ExecContext(ctx, `UPDATE ledger SET Money = Money + $1 WHERE ID = $2`, amount, to); err != nil {
+		return err
+	}
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
 }
